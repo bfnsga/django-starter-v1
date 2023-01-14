a5d6defed5adb69201a0efebf7ee746e3fbd67c1
Ben Field <bfnsga@gmail.com>
2023-01-14 17:24:06 -0500

    Testing GitHub Action workflow

--- app/tests.py
+++ app/tests.py
@@ -7,20 +7,3 @@ from django.urls import reverse
 
 # Custom User model
 User = get_user_model()
-
-class LoginTestCase(TestCase):
-    def test_login(self):
-        # Set up test environment and data
-        request = Mock()
-        request.build_absolute_uri.return_value = 'http://example.com/callback'
-        oauth = Mock()
-        oauth.auth0.authorize_redirect.return_value = '/dashboard'
-
-        # Call the code to be tested
-        response = login(request)
-
-        # Assert that the code is behaving as expected
-        self.assertEqual(response, '/dashboard')
-        oauth.auth0.authorize_params.assert_called_with({})
-        request.build_absolute_uri.assert_called_with(reverse('callback'))
-        oauth.auth0.authorize_redirect.assert_called_with(request, 'http://example.com/callback')
