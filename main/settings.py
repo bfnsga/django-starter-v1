f26d414ceb0b7011df39ee35953590bce30524f5
Ben Field <bfnsga@gmail.com>
2023-01-15 19:04:59 -0500

    Dcoker deployment troubleshooting

--- main/settings.py
+++ main/settings.py
@@ -48,7 +48,7 @@ else:
     DEBUG = True
     CSRF_COOKIE_SECURE = True
     SESSION_COOKIE_SECURE = True
-    SECURE_SSL_REDIRECT = False
+    SECURE_SSL_REDIRECT = True
     SECURE_HSTS_SECONDS = 60 # Change to 31536000 seconds (1 year) once confirmed in production
     SECURE_HSTS_INCLUDE_SUBDOMAINS = False
     SECURE_HSTS_PRELOAD = False
