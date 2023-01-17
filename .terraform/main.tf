4dce0507f3fe9750fe10cea44acc80242b720ff9
Ben Field <bfnsga@gmail.com>
2023-01-17 15:14:27 -0500

    Terraform update

--- .terraform/main.tf
+++ .terraform/main.tf
@@ -32,27 +32,27 @@ module "ecr" {
   source = "./modules/ecr"
 }
 
-#module "apprunner" {
-#  source = "./modules/apprunner"
-#
-#  ## Security Groups
-#  security_group-apprunner = module.vpc.security_group-apprunner
-#
-#  ## Subnets
-#  subnet_private_1 = module.vpc.subnet_private_1
-#  subnet_private_2 = module.vpc.subnet_private_2
-#
-#  ## AWS Metadata
-#  aws_account_id = var.aws_account_id
-#  aws_region = var.aws_region
-#
-#  ## AppRunner Environment Variables
-#  database_host = module.rds.database_host
-#  database_password = var.database_password
-#
-#  secret_key = var.secret_key
-#  auth0_client_id = var.auth0_client_id
-#  auth0_client_secret = var.auth0_client_secret
-#  auth0_domain = var.auth0_domain
-#  stripe_api_key = var.stripe_api_key
-#}
+module "apprunner" {
+  source = "./modules/apprunner"
+
+  ## Security Groups
+  security_group-apprunner = module.vpc.security_group-apprunner
+
+  ## Subnets
+  subnet_private_1 = module.vpc.subnet_private_1
+  subnet_private_2 = module.vpc.subnet_private_2
+
+  ## AWS Metadata
+  aws_account_id = var.aws_account_id
+  aws_region = var.aws_region
+
+  ## AppRunner Environment Variables
+  database_host = module.rds.database_host
+  database_password = var.database_password
+
+  secret_key = var.secret_key
+  auth0_client_id = var.auth0_client_id
+  auth0_client_secret = var.auth0_client_secret
+  auth0_domain = var.auth0_domain
+  stripe_api_key = var.stripe_api_key
+}
