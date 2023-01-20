c5ebf3aae0d6ed419430d98d05d9d9800b08592b
Ben Field <bfnsga@gmail.com>
2023-01-20 08:27:24 -0500

    URL updates troubleshooting

--- app_settings/urls.py
+++ app_settings/urls.py
@@ -3,14 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='login'),
-    path('settings/profile', views.dashboard, name='dashboard'),
     path('login', views.login, name='login'),
     path('logout', views.logout, name='logout'),
     path('signup', views.signup, name='signup'),
     path('callback', views.callback, name='callback'),
-    path('settings/password', views.password_change, name='password_change'),
+    path('settings/profile', views.dashboard, name='dashboard'),
     path('settings/users', views.users, name='users'),
-    path('users/add', views.add_user, name='add-user'),
-    path('users/delete', views.delete_user, name='delete_user'),
-    path('billing', views.billing, name='billing')
+    path('settings/billing', views.billing, name='billing'),
 ]
